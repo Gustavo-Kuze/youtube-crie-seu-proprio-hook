92c38b70c1a13d04d0b0e6500844f930d300e436
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-10-26 18:15:56 -0300

    mais alguns ajustes de estilo

--- usecontextmenu/src/components/Sobre.jsx
+++ usecontextmenu/src/components/Sobre.jsx
@@ -15,10 +15,7 @@ const Sobre = () => {
                 <h1>Sobre Este App</h1>
                 <p>{isContextMenuOpen ? "Aberto" : "fechado"}</p>
                 <p className={'texto'}>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Accusantium soluta fuga, sint dolores architecto optio vitae
-                    maxime necessitatibus? Sapiente non eum aspernatur
-                    dignissimos nemo dolorum accusamus unde iusto id aut?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore in nemo sed amet dignissimos sint, vero, obcaecati quasi neque praesentium tempore corporis eveniet totam, temporibus modi. Tempore molestias iusto pariatur blanditiis sequi ullam voluptatum nisi ab nemo aperiam, ipsum eveniet, quisquam, quaerat veritatis perspiciatis? Quis hic obcaecati omnis, dicta aperiam nesciunt sapiente voluptatibus velit excepturi? Velit quibusdam autem aperiam, delectus iste rem iusto obcaecati saepe. Minus dignissimos, sit facilis rem dolor distinctio nobis libero delectus nesciunt doloribus, suscipit possimus dolore.
                 </p>
             </div>
         </div>
