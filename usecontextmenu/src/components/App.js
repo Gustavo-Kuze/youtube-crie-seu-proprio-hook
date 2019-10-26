7faab48d5479f7766c0f4abffa5f91c17726f2dd
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-10-26 17:37:45 -0300

    projeto criado; componente inicial criado

---
+++ usecontextmenu/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+import Painel from './Painel';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Painel />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ usecontextmenu/src/components/Painel.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./styles.css";
+
+const Painel = () => {
+    const [isContextMenuOpen, setIsContextMenuOpen] = useState(false);
+
+    useEffect(() => {
+        document.oncontextmenu = () => setIsContextMenuOpen(true);
+        window.onclick = () => setIsContextMenuOpen(false);
+    }, []);
+
+    return (
+        <div className="panel" >
+            <p>{isContextMenuOpen ? "Aberto" : "fechado"}</p>
+        </div>
+    );
+};
+
+export default Painel;
