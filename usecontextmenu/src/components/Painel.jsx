39c1adfba869d31e2c692cfe130730d80b8d9719
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-10-26 18:42:10 -0300

    código pronto para o vídeo

--- usecontextmenu/src/components/Painel.jsx
+++ usecontextmenu/src/components/Painel.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import "./styles.css";
-import useContextMenuState from "../hooks/useContextMenuState";
 
 const Painel = () => {
-    const isContextMenuOpen = useContextMenuState(false);
-
     return (
-        <>
         <div className="panel">
-        <h1>Painel</h1>
-            <p>{isContextMenuOpen ? "Aberto" : "fechado"}</p>
+            <h1>Painel</h1>
+            <p>Aberto</p>
         </div>
-        </>
     );
 };
 
--- usecontextmenu/src/components/Sobre.jsx
+++ usecontextmenu/src/components/Sobre.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import "./styles.css";
-import useContextMenuState from "../hooks/useContextMenuState";
 
 const Sobre = () => {
-    const isContextMenuOpen = useContextMenuState(false);
-
     return (
-        <div>
-            <div className="panel">
-                <h1>Sobre Este App</h1>
-                <p>{isContextMenuOpen ? "Aberto" : "fechado"}</p>
-                <p className={'texto'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore in nemo sed amet dignissimos sint, vero, obcaecati quasi neque praesentium tempore corporis eveniet totam, temporibus modi. Tempore molestias iusto pariatur blanditiis sequi ullam voluptatum nisi ab nemo aperiam, ipsum eveniet, quisquam, quaerat veritatis perspiciatis? Quis hic obcaecati omnis, dicta aperiam nesciunt sapiente voluptatibus velit excepturi? Velit quibusdam autem aperiam, delectus iste rem iusto obcaecati saepe. Minus dignissimos, sit facilis rem dolor distinctio nobis libero delectus nesciunt doloribus, suscipit possimus dolore.
-                </p>
-            </div>
+        <div className="panel">
+            <h1>Sobre Este App</h1>
+            <p>Aberto</p>
+            <p className={"texto"}>
+                Este app se trata de um artifício tecnológico aplicado, outrora
+                conhecido como gambiarra, e tem como intuito lhe explicar
+                brevemente o funcionamento do ciclo de criação de um React hook.
+                Este por sua vez é apenas um parágrafo extremamente tedioso
+                feito apenas para completar espaço.
+            </p>
         </div>
     );
 };
